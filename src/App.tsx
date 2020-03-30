55e62b1969ea909978cf4b16ee7e11a89cd56d63
yutarofire <yutaro.ono.418@gmail.com>
2020-03-31 02:04:31 +0900

    Minimum Life game simulator

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,113 @@
 import React from 'react';
+import styled from 'styled-components';
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+type CellState = 0 | 1;
+
+class State {
+  cells: CellState[][] = [
+    [0, 1, 0],
+    [0, 1, 0],
+    [0, 1, 0],
+  ];
 }
 
-export default App;
+export default class App extends React.Component<{}, State> {
+  constructor(props: {}) {
+    super(props);
+    this.state = new State();
+  }
+
+  componentDidMount() {
+    setInterval(this.goToNextGeneration, 500);
+  }
+
+  render() {
+    const { cells } = this.state;
+
+    return (
+      <Wrapper>
+        {cells.map(cellRow => (
+          <Row>
+            {cellRow.map(cell => (
+              <Cell isLiving={cell === 1} />
+            ))}
+          </Row>
+        ))}
+      </Wrapper>
+    );
+  }
+
+  private goToNextGeneration = () => {
+    const { cells } = this.state;
+    const nextGenerationCells =
+      cells.map((cellRow, yAxisIndex) => {
+        return cellRow.map((cell, xAxisIndex) => {
+          const surroundingLivingCount = this.getSurroundingLivingCount(xAxisIndex, yAxisIndex);
+          return this.getNextState(
+            cell,
+            surroundingLivingCount,
+          );
+        });
+      });
+    this.setState({ cells: nextGenerationCells });
+  };
+
+  private getSurroundingLivingCount = (
+    xAxisIndex: number,
+    yAxisIndex: number,
+  ): number => {
+    const { cells } = this.state;
+    const surroundingCells = [
+      cells[yAxisIndex - 1]?.[xAxisIndex - 1],
+      cells[yAxisIndex - 1]?.[xAxisIndex],
+      cells[yAxisIndex - 1]?.[xAxisIndex + 1],
+      cells[yAxisIndex][xAxisIndex - 1],
+      cells[yAxisIndex][xAxisIndex + 1],
+      cells[yAxisIndex + 1]?.[xAxisIndex - 1],
+      cells[yAxisIndex + 1]?.[xAxisIndex],
+      cells[yAxisIndex + 1]?.[xAxisIndex + 1],
+    ];
+    return surroundingCells.filter(e => e === 1).length;
+  };
+
+  private getNextState = (
+    currentState: CellState,
+    surroundingLivingCellCount: number,
+  ): CellState => {
+    switch (currentState) {
+      case 0:
+        if (surroundingLivingCellCount === 3) {
+          return 1;
+        } else {
+          return 0;
+        }
+      case 1:
+        if (surroundingLivingCellCount <= 1 || surroundingLivingCellCount >= 4) {
+          return 0;
+        } else if (surroundingLivingCellCount === 2 || surroundingLivingCellCount === 3) {
+          return 1;
+        }
+      default:
+        throw new Error('never');
+    }
+  };
+}
+
+const Wrapper = styled.div`
+  padding: 10px;
+`;
+
+const cellWidth = 20;
+
+const Row = styled.div`
+  width: ${cellWidth * 3}px;
+  height: ${cellWidth}px;
+  display: flex
+`;
+
+const Cell = styled.div<{ isLiving: boolean }>`
+  width: ${cellWidth}px;
+  height: ${cellWidth}px;
+  background-color: ${({ isLiving }) => isLiving ? 'black' : 'white' };
+  border: solid 1px lightgray;
+`;
